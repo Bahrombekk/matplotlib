7b818fa322a63ba0bdd4d49629fab064c96215e2
Bahrom <muhammadiyevbahrom2002@gmail.com>
2023-02-15 09:50:55 +0500

    json

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Salom")
+import json
+file_path = "data.json"
+f=open(file_path)
+data_str=f.read()
+data=json.loads(data_str)
+values=data.values()
+a=data.get('a', 0)
+b=data.get('b',0)
+n={
+    "sum":a+b
+} 
+adf=json.dumps(n, indent=4)
+f1=open("result.json", "w")
+f1=f1.write(adf)
+# test
+
